6c248075912895aa61fd65534bd4048eb6ecd07b
unknown <alngregory@gmail.com>
2022-01-05 22:59:26 -0300

    Update database pelo EF feito com sucesso

---
+++ Migrations/20220105014947_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc_rest_api_fiz_hoje.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FizHoje",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaFeito = table.Column<bool>(type: "bit", nullable: false),
+                    DataFeito = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FizHoje", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FizHoje");
+        }
+    }
+}
