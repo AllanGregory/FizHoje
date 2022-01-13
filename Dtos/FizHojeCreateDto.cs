75cee32ffae56f76b3904cf06c19f38d817bba90
unknown <alngregory@gmail.com>
2022-01-12 22:26:37 -0300

    Colocando annotation de required na classe DTO para retornar erro amigavel no request

--- Dtos/FizHojeCreateDto.cs
+++ Dtos/FizHojeCreateDto.cs
@@ -4,7 +4,10 @@ namespace FizHoje.Dtos
 {
     public class FizHojeCreateDto
     {
+        [Required]
         public bool TaFeito { get; set; }
+
+        [Required]
         public string DataFeito { get; set; }
     }
 }
